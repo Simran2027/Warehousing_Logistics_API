aadcb7c897e32388d2610ec1185a072cf7a28910
Simran <simran2027b@gmail.com>
2024-07-14 18:47:17 +0530

    initial commit

---
+++ src/main/java/com/api/warehousing_logistics_api/WarehousingLogisticsApiApplication.java
@@ -0,0 +1,13 @@
+package com.api.warehousing_logistics_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarehousingLogisticsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarehousingLogisticsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/warehousing_logistics_api/WarehousingLogisticsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.warehousing_logistics_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarehousingLogisticsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
